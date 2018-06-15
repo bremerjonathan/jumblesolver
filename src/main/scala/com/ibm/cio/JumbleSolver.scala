f209699d1faec421b3c7abf8c2f5d687ddd17f84
Bremer Jonathan <bremer.jonathan@gmail.com>
2018-06-15 14:49:19 -0400

    added word frequency dictionary resource

--- src/main/scala/com/ibm/cio/JumbleSolver.scala
+++ src/main/scala/com/ibm/cio/JumbleSolver.scala
@@ -1,5 +1,14 @@
 package com.ibm.cio
 
+import org.json4s.jackson.JsonMethods.parse
+
 object JumbleSolver {
+
+  lazy val wordFrequencyJson = scala.io.Source.fromInputStream(getClass.getClassLoader.getResourceAsStream("freq_dict.json")).mkString
+  lazy val wordFrequencyMap  = parse(wordFrequencyJson).values.asInstanceOf[Map[String, BigInt]]
+
+  def main(args: Array[String]): Unit = {
+    wordFrequencyMap.foreach(a => println(a))
+  }
 
 }
