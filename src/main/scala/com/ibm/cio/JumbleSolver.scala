4a6ccc2900c90a97d00899d115ffcf3435b91fda
Bremer Jonathan <bremer.jonathan@gmail.com>
2018-06-15 23:52:57 -0400

    implemented jumble unscrambling functionality

--- src/main/scala/com/ibm/cio/JumbleSolver.scala
+++ src/main/scala/com/ibm/cio/JumbleSolver.scala
@@ -1,13 +1,20 @@
 package com.ibm.cio
 
+import com.ibm.cio.JumbleSolver.{createJumbleTuplesUDF, unscrambleJumble, unscrambleJumblesUDF}
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.udf
+import org.apache.spark.sql.{Row, SparkSession}
 import org.json4s.jackson.JsonMethods.parse
+
+import scala.util.control.Breaks.{break, breakable}
 
 object JumbleSolver {
 
   lazy val wordFrequencyJson = scala.io.Source.fromInputStream(getClass.getClassLoader.getResourceAsStream("freq_dict.json")).mkString
   lazy val wordFrequencyMap  = parse(wordFrequencyJson).values.asInstanceOf[Map[String, BigInt]]
+
+  val createJumbleTuplesUDF = udf(createJumbleTuples _)
+  val unscrambleJumblesUDF  = udf(unscrambleJumbles _)
 
   def main(args: Array[String]): Unit = {
     //TODO: unset master from local
@@ -18,12 +25,47 @@ object JumbleSolver {
       .option("ignoreTrailingWhiteSpace","true")
       .option("escape", "\"")
       .option("header", "true")
-      //TODO: unhardcord input path. Get it as the 1st command line argument.
+      //TODO: unhardcode input path. Get it as the 1st command line argument.
       .csv("input/*.csv")
 
-    puzzle.printSchema()
-    puzzle.show(false)
+    val jumbleTuples       = puzzle.withColumn("jumble_tuples", createJumbleTuplesUDF(puzzle("jumbles")))
+    val unscrambledJumbles = jumbleTuples.withColumn("unscrambled_jumbles", unscrambleJumblesUDF(jumbleTuples("jumble_tuples"))).drop("jumble_tuples")
 
+    unscrambledJumbles.show(false)
+
+  }
+
+  // Converts a string of jumbles in the format "abc(1,2),de(3,4)") to an array of tuples [(abc,[1 ,2, 3]), (de,[3,4])]
+  def createJumbleTuples(jumbles: String): Seq[(String, Seq[Int])] = {
+    jumbles.split("\\)\\s*,\\s*")
+      .map( x => (x.replaceAll("\\((.)*", ""), x.replaceAll("([^\\(])*\\(|(\\D)*\\)", "")) )
+      .map(y => (y._1, y._2.split("\\s*,\\s*").map(_.toInt).toSeq))
+  }
+
+  def unscrambleJumbles(jumbles: Seq[Row]): Seq[(String, Seq[Int])] = {
+    jumbles.map(x => {
+      (unscrambleJumble(x.getAs[String]("_1"))._1, x.getAs[Seq[Int]]("_2"))
+    })
+  }
+
+  // This method the most frequent permutation word from the word frequency map (i.e. wordFrequencyMap)
+  def unscrambleJumble(jumble: String): (String, BigInt) = {
+    var word: String    = null
+    var wordFrequency: BigInt = -1
+
+    breakable {
+      for (permutation: String <- jumble.toLowerCase.permutations if wordFrequencyMap.contains(permutation)) {
+        if (wordFrequencyMap(permutation) == 1) {
+          word = permutation
+          break
+        }
+        if (wordFrequency < wordFrequencyMap(permutation)) {
+          word = permutation
+          wordFrequency = wordFrequencyMap(permutation)
+        }
+      }
+    }
+    (word, wordFrequency)
   }
 
 }
